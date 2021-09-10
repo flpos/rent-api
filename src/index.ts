dc64cef446cf5b3b0db3f6203034ef36ba200c9d
Felipe Oliveira <felipe_ods@outlook.com>
2021-09-10 11:08:45 -0300

    feat: add cors

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import { CarMongodbRepository } from './data/remote/car-mongodb.repository';
 import { RentMongodbRepository } from './data/remote/rent.mongodb.repository';
 import { UserMongodbRepository } from './data/remote/user-mongodb.repository';
 import { setupControllers } from './presentation/setupControllers';
+import cors from 'cors';
 
 const databaseUri =
   process.env.DB_URL || 'mongodb://root:example@localhost:27017';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const mongoClient = new MongoClient(databaseUri);
 const db = mongoClient.db('rent');
